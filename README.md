8d1fcb2bbe402d04a2295b49efd4ef44ff39d85e
sedrubal <dev@sedrubal.de>
2016-07-16 01:32:07 +0200

    Der erste commit
    Der gäit.

---
+++ README.md
@@ -0,0 +1,21 @@
+Der Skript
+==========
+
+Der Javascript zu der Ersetzen von Sprache in der kapotte Sprache in der Website.
+---------------------------------------------------------------------------------
+
+Inspiriert von der [Vale](https://github.com/vale/).
+
+Der Verwendung
+--------------
+
+Der Developer Console in der Firefox oder der Chrome öffnen und der [Skript](derScript.js) rein copy&pasten.
+
+License
+-------
+
+DER BEER-WARE LIZENZ (Revision 1337):
+Der [Sedrubal](https://github.com/sedrubal) hat der Projekt gemacht.
+Solange du der Lizenzhinweis beibehältst, kannst du damit machen was du willst.
+Wenn wir uns irgendwann mal treffen und du denkst, dass der Zeug es wert ist, kannst du mir der Bier kaufen.
+Sedrubal
---
+++ derSkript.js
@@ -0,0 +1,74 @@
+var derKeyWorte = {
+  'die ': 'der ',
+  'das ': 'der ',
+  'zum ': 'zu der ',
+  'zur ': 'zu der ',
+  'ein ': 'der ',
+  'eine ': 'der ',
+  'eines ': 'der ',
+  'einer ': 'der ',
+  'einem ': 'der ',
+  'einen ': 'der ',
+  'den ': 'der ',
+  'dem ': 'der ',
+  'am ': 'an der ',
+  'ihre ': 'der ',
+  'ihren ': 'der ',
+  'ihrer ': 'der ',
+  'ihres ': 'der ',
+  'im ': 'in der ',
+  'ins ': 'in der ',
+  'diese ': 'der ',
+  'dieser ': 'der ',
+  'dieses ': 'der ',
+  'diesen ': 'dem ',
+  'diesem ': 'dem ',
+  'für (?!der)': 'für der ',
+  'computer': 'kombjudoor',
+  'auto ': 'autowagen '
+}
+
+/* der Text Knoten zurueckbekommen */
+function getAllTextNodes(){
+  var result = [];
+
+  (function scanSubTree(node){
+    if(node.childNodes.length) 
+      for(var i = 0; i < node.childNodes.length; i++) 
+        scanSubTree(node.childNodes[i]);
+    else if(node.nodeType == Node.TEXT_NODE) 
+      result.push(node);
+  })(document);
+
+  return result;
+}
+
+/* escape der regexp */
+function quote(str){
+  return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+}
+
+/* der ersten Buchstaben groß machen */
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+/* der Text ersetzen */
+function derReplace(from, to){
+  getAllTextNodes().forEach(function(node){
+    node.nodeValue = node.nodeValue.replace(new RegExp(quote(' '+from), 'g'), ' '+to);
+    node.nodeValue = node.nodeValue.replace(new RegExp(capitalize(quote(from)), 'g'), capitalize(to));
+  });
+}
+
+/* der Funktion */
+function derFunktion() {
+  for (var der in derKeyWorte) {
+    if (derKeyWorte.hasOwnProperty(der)) {
+      var derKapott = derKeyWorte[der];
+      derReplace(der, derKapott);
+    }
+  }
+}
+
+derFunktion();
